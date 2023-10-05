94469214e4060dddcce0663518e662b54dedf315
Replit user <>
2023-10-05 19:58:47 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,6 +51,7 @@ int is_valid(Node* n) //validar si un estado/nodo es válido
   
   int arregloFilas[10] = {};
   int arregloCols[10] = {};
+  return 1;
   //int arregloSubm[10] = {};
   //recorro matriz
   for(int i = 0 ; i < 9 ; i++){
